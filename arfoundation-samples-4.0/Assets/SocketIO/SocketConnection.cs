2c22053bd34db78e22a6948bc16a4140ea5b1751
Adriaan Wormgoor (bombarie) <bombarie@gmail.com>
2021-05-20 12:17:23 +0200

    streamline connecting to Socket Server

--- arfoundation-samples-4.0/Assets/SocketIO/SocketConnection.cs
+++ arfoundation-samples-4.0/Assets/SocketIO/SocketConnection.cs
@@ -6,75 +6,98 @@ using Socket.Quobject.SocketIoClientDotNet.Client;
 using UnityEngine;
 
 public class SocketConnection : MonoBehaviour {
-    private QSocket socket;
-    public string url = "";
-    public int portNum = 3000;
+   private QSocket socket;
+   public string url = "";
+   public int portNum = 3000;
 
-    void Start() {
-        Debug.Log("start >> url: " + url);
-        string _url;
-        if (url != "") {
-            if (!url.StartsWith("http://")) {
-                url = "http://" + url;
+   private static SocketConnection _instance;
+   public static SocketConnection instance {
+      get {
+         if (_instance == null) {
+            _instance = GameObject.FindObjectOfType<SocketConnection>();
+         }
+
+         return _instance;
+      }
+   }
+
+
+   void Start() {
+      //Debug.Log("start >> url: " + url);
+      //connect(url);
+   }
+
+   public void connect(string url) {
+      Debug.Log("SocketConnection >> f:connect >> url: " + url);
+
+      if (socket != null) {
+         socket.Disconnect();
+      }
+
+      string _url;
+      if (url != "") {
+         if (!url.StartsWith("http://")) {
+            url = "http://" + url;
+         }
+         _url = url + ":" + portNum.ToString();
+      } else {
+         _url = "http://localhost:3000";
+      }
+      Debug.Log("SocketConnection >> f:connect >> connecting to url '" + _url + "'");
+
+      socket = IO.Socket(_url);
+
+      socket.On(QSocket.EVENT_CONNECT, () => {
+         Debug.Log("Connected");
+         socket.Emit("chat", "test");
+      });
+
+      socket.On("fnameNew", data => {
+         Debug.Log("fnameNew : " + data);
+         addBoid = true;
+      });
+   }
+
+   private bool addBoid = false;
+   void Update() {
+      if (addBoid) {
+         try {
+            int i = Random.Range(0, 3);
+            switch (i) {
+               case 0:
+                  if (BoidsAddRemover.instance != null) {
+                     BoidsAddRemover.instance.addBoidType(BoidsAddRemover.Type.TYPE_1);
+                  }
+                  break;
+               case 1:
+                  if (BoidsAddRemover.instance != null) {
+                     BoidsAddRemover.instance.addBoidType(BoidsAddRemover.Type.TYPE_2);
+                  }
+                  break;
+               case 2:
+                  if (BoidsAddRemover.instance != null) {
+                     BoidsAddRemover.instance.addBoidType(BoidsAddRemover.Type.TYPE_3);
+                  }
+                  break;
             }
-            _url = url + ":" + portNum.ToString();
-        } else {
-            _url = "http://localhost:3000";
-        }
-        Debug.Log("connecting to url '" + _url + "'");
-        socket = IO.Socket(_url);
+         } catch (System.Exception e) {
+            Debug.LogWarning("ERROR while adding new Boid type: " + e.ToString());
+         }
 
-        socket.On(QSocket.EVENT_CONNECT, () => {
-            Debug.Log("Connected");
-            socket.Emit("chat", "test");
-        });
+         addBoid = false;
+      }
+      if (Input.GetKeyUp(KeyCode.Space)) {
+         Debug.Log("spacebar >> registering name 'Frank'");
 
-        socket.On("fnameNew", data => {
-            Debug.Log("fnameNew : " + data);
-            addBoid = true;
-        });
-    }
+         if (socket != null) {
+            socket.Emit("fname", "Frank");
+         }
+      }
+   }
 
-    private bool addBoid = false;
-    void Update() {
-        if (addBoid) {
-            try {
-                int i = Random.Range(0, 3);
-                switch (i) {
-                    case 0:
-                        if (BoidsAddRemover.instance != null) {
-                            BoidsAddRemover.instance.addBoidType(BoidsAddRemover.Type.TYPE_1);
-                        }
-                        break;
-                    case 1:
-                        if (BoidsAddRemover.instance != null) {
-                            BoidsAddRemover.instance.addBoidType(BoidsAddRemover.Type.TYPE_2);
-                        }
-                        break;
-                    case 2:
-                        if (BoidsAddRemover.instance != null) {
-                            BoidsAddRemover.instance.addBoidType(BoidsAddRemover.Type.TYPE_3);
-                        }
-                        break;
-                }
-            } catch (System.Exception e) {
-                Debug.LogWarning("ERROR while adding new Boid type: " + e.ToString());
-            }
-
-            addBoid = false;
-        }
-        if (Input.GetKeyUp(KeyCode.Space)) {
-            Debug.Log("spacebar >> registering name 'Frank'");
-
-            if (socket != null) {
-                socket.Emit("fname", "Frank");
-            }
-        }
-    }
-
-    private void OnDestroy() {
-        if (socket != null) {
-            socket.Disconnect();
-        }
-    }
+   private void OnDestroy() {
+      if (socket != null) {
+         socket.Disconnect();
+      }
+   }
 }
